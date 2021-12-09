befed277fb873a871e522f6a3479017503532dbf
thearol <thearollesloth@hotmail.com>
2021-12-09 15:54:00 +0100

    plot function

--- scripts/viz.py
+++ scripts/viz.py
@@ -2,27 +2,34 @@ import numpy as np
 from gensim.models.keyedvectors import KeyedVectors
 import matplotlib.pyplot as plt
 import pandas as pd
+import json
+from numpy import loadtxt
+if sys.version_info[0] < 3:
+    import io
+    open = io.open
 
 model = KeyedVectors.load_word2vec_format('/work/dagw_wordembeddings/word2vec_model/DAGW-model.bin', binary=True) 
 #k_model_w2v = KeyedVectors.load_word2vec_format('/work/dagw_wordembeddings/word2vec_model/debiased_model.bin', binary=True) 
-Wl = ['ude', 'hjemme','rig', 'fattig', 'dronning', 'konge', 'skuespillerinde', 'skuespiller']
-from numpy import loadtxt
-b2 = np.loadtxt('/work/Exam/dk-weat/output/neutral_specific_difference.csv', delimiter=',')
- xx = ['mand', 'kvinde']
+#Wl = ['ude', 'hjemme','rig', 'fattig', 'dronning', 'konge', 'skuespillerinde', 'skuespiller']
+
+Wl = ['revisor', 'sygeplejerske','prinsesse', 'dronning','skuespiller', 'skuespillerinde', 'advokat', 'hjælper', 'ambassadør', 'analytiker', 'antropolog', 'arkæolog', 'ærkebiskop', 'arkitekt', 'kunstner', 'kunstner', 'morder ', 'assistent_professor', 'associate_dean', 'associate_professor', 'astronaut', 'astronom ', 'atlet', 'atletisk_direktør', 'advokat', 'forfatter', 'bager ', 'ballerina', 'boldspiller', 'bankmand', 'barber', 'baron', 'advokat', 'bartender', 'biolog', 'biskop', 'livvagt', 'bogholder', 'chef', 'bokser', 'broadcaster', 'mægler', 'bureaukrat', 'forretningsmand', 'forretningskvinde', 'slagter ', 'butler', 'cab_driver', 'cabbie', 'kameramand', 'kampagneleder', 'kaptajn', 'kardiolog', 'plejer', 'tømrer', 'tegner', 'cellist', 'kansler', 'præst', 'karakter', 'kok', 'kemiker', 'koreograf', 'biograf', 'borger', 'embedsmand', 'præst', 'funktionær', 'træner', 'samler', 'oberst', 'klummeskribent', 'komiker', 'komiker', 'kommandør', 'kommentator', 'kommissær', 'komponist', 'dirigent', 'indrømmer', 'kongresmedlem', 'konstabel', 'konsulent', 'politi', 'korrespondent', 'rådmand', 'rådmand', 'rådgiver', ' kritiker', 'crooner', 'korsfarer', 'kurator', 'formynder', 'far', 'danser', 'dekan', 'tandlæge', 'stedfortræder', 'hudlæge', 'detektiv', 'diplomat', 'instruktør', 'disc_jockey', 'læge', 'doktorand', 'narkoman', 'trommeslager', 'økonomiprofessor', 'økonom', 'redaktør', 'pædagog', 'elektriker', 'medarbejder', 'entertainer', 'iværksætter', 'miljøforkæmper', 'udsending', 'epidemiolog', 'evangelist', 'bonde', 'modedesigner', 'fighter_pilot', 'filmskaber', 'finansmand', 'brandmand', 'brandmand', 'brandmand', 'fisker', 'fodboldspiller', 'foreman', 'freelance_writer', 'gangster', 'gartner', 'geolog', 'målmand', 'grafisk_designer', 'vejleder', 'guitarist', 'frisør', 'handyman', 'skoleleder', 'historiker', 'hitman', 'homemaker', 'hooker', 'husholderske', 'husmor', 'illustratør', 'industriist', 'infielder', 'inspektør', 'instruktør', 'interior_designer', 'opfinder', 'investigator', 'investment_banker', 'pedel', 'juveler', 'journalist', 'dommer', 'jurist', 'arbejder', 'udlejer', 'lovgiver', 'advokat', 'foredragsholder', 'lovgiver', 'bibliotekar', 'løjtnant', 'livredder', 'lyriker', 'maestro', 'magiker', 'magistrat', 'maid', 'major_leaguer', 'manager', 'skytte', 'marshal', 'matematiker', 'mekaniker', 'mediator', 'læge', 'midtbanespiller', 'minister', 'missionær', 'gangster', 'munk', 'musiker', 'barnepige', 'fortæller', 'naturalist', 'forhandler', 'neurolog', 'neurokirurg', 'romanforfatter', 'nunn', 'sygeplejerske', 'observatør', 'officer', 'organist', 'maler', ' advokatfuldmægtig', 'sognebarn', 'parlamentariker', 'præst', 'patolog', 'patruljemand', 'børnlæge', 'performer', 'farmaceut', 'filantrop', 'filosof', 'fotograf', 'fotojournalist', 'læge', 'fysiker', 'pianist', 'planlægger', 'plastikkirurg', 'dramatiker', 'blikkenslager', 'digter', 'politimand', 'politiker', 'pollster', 'prædikant', 'præsident', 'præst', 'rektor', 'fange', 'professor', 'professor_emeritus', 'programmør', 'promotor', 'ejer', 'anklager', 'hovedperson', 'protege', 'demonstrerende', 'prost', 'psykiater', 'psykolog', 'publicist', 'pundit', 'rabbiner', 'radiolog', 'ranger', 'mægler', 'receptionist', 'registreret_sygeplejerske', 'forsker', 'restauratør', 'sømand', 'helgen', 'sælger', 'saxofonist', 'lærd', 'videnskabsmand', 'manuskriptforfatter', 'billedhugger', 'sekretær', 'senator', 'sergent', 'tjener', 'servicemand', 'sheriff_deputy', 'butiksejer', 'sanger', 'singer_songwriter', 'skipper', 'socialite', 'sociolog', 'soft_spoken', 'soldat', 'advokat', 'advokat_general', 'solist', 'sportsmand', 'sportsskriver', 'statsmand', 'steward', 'børsmægler', 'strateg', 'elev', 'stylist', 'substitut', 'superintendent', 'kirurg', 'surveyor', 'svømmer', 'taxichauffør', 'lærer', 'tekniker', 'teenager', 'terapeut', 'handler', 'kasserer', 'trooper', 'trucker', 'trompetist', 'tutor', 'tycoon', 'undersekretær', 'understudy', 'valedictorian', 'vice_chancellor', 'violinist', 'vokalist', 'tjener', 'servitrice', 'vagt', 'kriger', 'svejser', 'arbejder', 'wrestler', 'writer']
+
+y_ax = np.loadtxt('/work/Exam/dk-weat/output/neutral_specific_difference.csv', delimiter=',')
+x_ax = ['han', 'hun']
 
 def plot_professions(embedding, wordlist, x_axis, y_axis):
 
     vectors = []
 
     words = x_axis + wordlist
-    
+    #words =[w for w in words if w in embedding[words]]
     for i in range(len(words)):
         # Embeddings
         vectors.append(embedding[words[i]])
     # To-be basis
 
     x = (vectors[1]-vectors[0])
-    y = (vectors[3]-vectors[2])
+    y = y_axis
     
     # Get pseudo-inverse matrix
     W = np.array(vectors)
@@ -44,10 +51,11 @@ def plot_professions(embedding, wordlist, x_axis, y_axis):
     ax.scatter(df['x'], df['y'])
     ax.set_ylabel('genderedness')
     ax.set_xlabel('difference of mand-kvinde')
+    ax.axvline(x=0.5)
     # add word label to each point
     for w, pos in df.iterrows():
         ax.annotate(w, pos)
 
     return ax.plot()
 
-plot_professions(model, Wl,xx, b2)
+plot_professions(model, Wl[:6],x_ax, y_ax)
