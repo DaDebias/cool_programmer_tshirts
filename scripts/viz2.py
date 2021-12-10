41c8355d719fbe978681ee3a39019f477f2109a7
thearol <thearollesloth@hotmail.com>
2021-12-10 13:31:45 +0100

    update plot

--- scripts/viz2.py
+++ scripts/viz2.py
@@ -13,7 +13,7 @@ plt.style.use("seaborn")
 model = KeyedVectors.load_word2vec_format('/work/dagw_wordembeddings/word2vec_model/DAGW-model.bin', binary=True) 
 debiased_model = KeyedVectors.load_word2vec_format('/work/dagw_wordembeddings/word2vec_model/debiased_model.bin', binary=True)
 
-wordlist = ['revisor', 'sygeplejerske','prinsesse', 'dronning','skuespiller', 'skuespillerinde', 'advokat', 'hjælper',  'antropolog', 'arkæolog', 'arkitekt', 'kunstner',  'morder', 'astronaut', 'astronom', 'atlet',  'forfatter', 'bager', 'ballerina', 'fodboldspiller', 'bankmand', 'barber', 'baron', 'bartender', 'biolog', 'biskop', 'præst']
+wordlist = ['søster', 'bror','tvillingebror', 'tvillingesøster','revisor', 'sygeplejerske','prinsesse', 'dronning','skuespiller', 'skuespillerinde', 'advokat', 'hjælper',  'antropolog', 'arkæolog', 'arkitekt', 'kunstner',  'morder', 'astronaut', 'astronom', 'atlet',  'forfatter', 'bager', 'ballerina', 'fodboldspiller', 'bankmand', 'barber', 'baron', 'bartender', 'biolog', 'biskop', 'præst']
 x_ax = ['kvinde', 'mand']
 y_ax = np.loadtxt('/work/Exam/dk-weat/output/neutral_specific_difference.csv', delimiter=',')
 
@@ -29,7 +29,7 @@ def plot_professions(embedding, wordlist, x_axis, y_axis):
     # To-be basis
     x = (vectors[1]-vectors[0])
     #flipped
-    y = np.flipud(y_axis)
+    y = y_axis #np.flipud(y_axis)
     #reversed y axis
     #y = (vectors[3]-vectors[2]) #np.loadtxt('/work/Exam/dk-weat/output/neutral_specific_difference.csv', delimiter=',')
 
@@ -63,8 +63,7 @@ def plot_professions(embedding, wordlist, x_axis, y_axis):
 
     #plt.ylim([-0.0005, 0.20])
     for i, txt in enumerate(wordlist):
-        #plt.annotate(txt, (Wp[0,i]+rX*eps, Wp[1,i]+rX*eps))
-        plt.annotate(txt, (Wp[0,i]+rX*eps, Wp[1,i]+rY*eps))
+        plt.annotate(txt, (Wp[0,i]+rX*eps, Wp[1,i]+rY*eps)) # changed from #plt.annotate(txt, (Wp[0,i]+rX*eps, Wp[1,i]+rX*eps))
     plt.show()
 
 plot_professions(model, wordlist, x_ax, y_ax)
